f727fcd7f71c3e13fc5a72726ae4d5d206cc098e
narozhniakmarian <narozhniakmarian@gmail.com>
2025-08-07 21:40:47 +0200

    merge producrList&modalDetailis

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
-export const refs = {
-    modalDetailisProduct: document.querySelector('.modal'),
-};
-=======
 const refs = {
   feedbackList: document.querySelector('.feedback-list'),
   categoriesList: document.querySelector('.furniture-categories'),
   furnitureGrid: document.querySelector('.furniture-gallery'),
   furnitureLoadMoreBtn: document.querySelector('#furniture-loadMoreBtn'),
+  modalDetailisProduct: document.querySelector('.modal'),
 };
 
 export default refs;
->>>>>>> origin/main
--- src/main.js
+++ src/main.js
@@ -1,22 +1,8 @@
 import 'modern-normalize/modern-normalize.css';
 import './css/styles.css';
-<<<<<<< HEAD
 import 'izitoast/dist/css/iziToast.min.css';
 import iziToast from 'izitoast';
-import refs from './js/refs.js';
-// import { getFurnitureList, furnitureData } from './Богдан-файл.js';  назви потім відкорегую
-import { setupProductClickHandler } from './product-modal-handler.js';
 
-async function init() {
-    await getFurnitureList();
-    const allProducts = [...furnitureData];
-
-    renderListProduct(allProducts); // рендеримо список (назву теж треба буде підшаманити)
-    setupProductClickHandler(allProducts); // модуль модалки
-}
-
-init();
-=======
 import { feedbackSection } from './js/feedback';
 import { handlerCategories } from './js/furniture_list';
 import refs from './js/refs';
@@ -24,4 +10,4 @@ import refs from './js/refs';
 feedbackSection();
 // Furniture-list
 refs.categoriesList.addEventListener('click', handlerCategories);
->>>>>>> origin/main
+
