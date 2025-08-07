b0cc43c46685ad88e0d32bee13a83a155128029c
Roman Panchuk <panchuk.roman98@gmail.com>
2025-08-07 19:20:45 +0200

    Fixed furniture list section

--- src/js/furniture_list.js
+++ src/js/furniture_list.js
@@ -1,133 +1,108 @@
-
-import axios from "axios";
+import axios from 'axios';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-import '../css/furniture-list.css'
+import '../css/furniture-list.css';
+import refs from './refs';
 
-const BaseUrl = "https://furniture-store.b.goit.study/api/";
-const categoriesList = document.querySelector(".categories");
-
-const furnitureGrid = document.querySelector(".gallery");
-const loadMoreBtn = document.querySelector("#loadMoreBtn");
-
+const BaseUrl = 'https://furniture-store.b.goit.study/api/';
 
 export async function getCategories() {
-    try {
-    
+  try {
     const res = await axios.get(`${BaseUrl}categories`);
     const categories = res.data;
     markUpCategories(categories);
-    
-    } catch (error) {
-       iziToast.error({
-    title: 'Помилка',
-    message: 'Не вдалося завантажити дані. Спробуйте пізніше',
-    position: 'topRight',
-  });
-        
-    }
-   
-    
-};
-
-function markUpCategories(categories) {
-    const markUp = [
-        {_id:"", name: "Всі товари" },
-        ...categories,
-    ]
-    .map(({_id, name}) => 
-    
-    `<li><button type="button" class="category-btn${_id === "" ? " active" : ""}"  data-category="${_id}">${name}</button></li>`  
-   
-    
-    ).join("");
-categoriesList.insertAdjacentHTML("beforeend", markUp)
+  } catch (error) {
+    iziToast.error({
+      title: 'Помилка',
+      message: 'Не вдалося завантажити дані. Спробуйте пізніше',
+      position: 'topRight',
+    });
+  }
 }
 
-
-
-
-
+function markUpCategories(categories) {
+  const markUp = [{ _id: '', name: 'Всі товари' }, ...categories]
+    .map(
+      ({ _id, name }) =>
+        `<li><button type="button" class="category-btn${
+          _id === '' ? ' active' : ''
+        }"  data-category="${_id}">${name}</button></li>`
+    )
+    .join('');
+  refs.categoriesList.insertAdjacentHTML('beforeend', markUp);
+}
 
 let page = 1;
 const limit = 8;
-export async function getFurniture(limit, page, category = "") {
-    try{
+export async function getFurniture(limit, page, category = '') {
+  try {
     const params = {
-        limit: limit,
-        page: page,
-        
+      limit: limit,
+      page: page,
     };
     if (category) {
-        params.category = category
-    };
-    
-    const responce = await axios.get(`${BaseUrl}furnitures` , {params})
-    const data = responce.data;
-    
-    if(page === 1) {
-        furnitureGrid.innerHTML = "";
+      params.category = category;
+    }
 
+    const responce = await axios.get(`${BaseUrl}furnitures`, { params });
+    const data = responce.data;
+
+    if (page === 1) {
+      refs.furnitureGrid.innerHTML = '';
     }
     markUpFurniture(data.furnitures);
-     if (page * limit >= data.totalItems) {
-      loadMoreBtn.style.display = "none";
+    if (page * limit >= data.totalItems) {
+      refs.furnitureLoadMoreBtn.style.display = 'none';
     } else {
-      loadMoreBtn.style.display = "block";
+      refs.furnitureLoadMoreBtn.style.display = 'block';
     }
-
-
-} catch (error) {
+  } catch (error) {
     iziToast.error({
-    title: 'Помилка',
-    message: 'Не вдалося завантажити дані. Спробуйте пізніше',
-    position: 'topRight',
-  });
-    
+      title: 'Помилка',
+      message: 'Не вдалося завантажити дані. Спробуйте пізніше',
+      position: 'topRight',
+    });
+  }
 }
-}
-
 
 function markUpFurniture(items) {
-    const markUp = items
-    .map(({_id, name, images, color, price}) => {
-        const colorsFurniture = color
-    .map(c => `<span class="color-dot" style="background-color:${c}"></span>`)
-    .join("");
-    
-       return `
+  const markUp = items
+    .map(({ _id, name, images, color, price }) => {
+      const colorsFurniture = color
+        .map(
+          c => `<span class="color-dot" style="background-color:${c}"></span>`
+        )
+        .join('');
+
+      return `
         <li class="furniture-card">
         <img src="${images[0]}" alt="${name}" class="furniture-img">
         <h3 class="furniture-name">${name}</h3>
         
         <p class="furniture-color"> ${colorsFurniture}</p>
         <p class="furniture-price">${price} грн</p>
-        <button class="btn btn-details" data-id="${_id}">Детальніше</button>
+        <button class="furniture-btn btn-details" data-id="${_id}">Детальніше</button>
 
         </li>
         
-        `;}).join("");
-        furnitureGrid.insertAdjacentHTML("beforeend", markUp);
+        `;
+    })
+    .join('');
+  refs.furnitureGrid.insertAdjacentHTML('beforeend', markUp);
 }
 getCategories();
 getFurniture(limit, page);
 
+export function handlerCategories(event) {
+  if (!event.target.classList.contains('category-btn')) return;
+  const buttons = refs.categoriesList.querySelectorAll('.category-btn');
+  buttons.forEach(btn => btn.classList.remove('active'));
 
-categoriesList.addEventListener("click", handlerCategories) ;
+  event.target.classList.add('active');
 
-function handlerCategories(event) {
+  const selectCategory = event.target.dataset.category;
 
-if(!event.target.classList.contains("category-btn")) return;
-const buttons = categoriesList.querySelectorAll(".category-btn");
-buttons.forEach(btn => btn.classList.remove("active"));
+  page = 1;
 
-event.target.classList.add("active");
-
-const selectCategory = event.target.dataset.category;
-
-page = 1;
-
-getFurniture(limit, page, selectCategory)
-
+  getFurniture(limit, page, selectCategory);
 }
-
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,5 +1,8 @@
 const refs = {
   feedbackList: document.querySelector('.feedback-list'),
+  categoriesList: document.querySelector('.furniture-categories'),
+  furnitureGrid: document.querySelector('.furniture-gallery'),
+  furnitureLoadMoreBtn: document.querySelector('#furniture-loadMoreBtn'),
 };
 
 export default refs;
