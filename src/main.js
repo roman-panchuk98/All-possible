160a23201f812e3266f529cadd8e348632f15ad2
Yaroslav Hoian <sgoyan1405@gmail.com>
2025-08-07 18:03:28 +0300

    fix function feedback in main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import 'modern-normalize/modern-normalize.css';
 import './css/styles.css';
-import { getFeedback } from './js/product-api';
 import { feedbackSection } from './js/feedback';
-
-getFeedback();
 
 feedbackSection();
