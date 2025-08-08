b25597db62589e58cc6a5aeef785c15aa41c46ca
yarynkaaaa <146334331+yarynkaaaa@users.noreply.github.com>
2025-08-08 19:08:52 +0300

    done faq

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,7 +1,9 @@
 import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
 
-new Accordion('.accordion-container', {
-  triggerClass: 'accordion-btn-question',
-  panelClass: 'accordion-show-answer',
+document.addEventListener('DOMContentLoaded', () => {
+  new Accordion('.accordion-container', {
+    duration: 1000,
+    showMultiple: false,
+    collapse: true,
+  });
 });
