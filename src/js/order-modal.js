2b729351080002a030bce0d8f22ee5e9d3d983c0
Anastasiia <gorgona1597531597@gmail.com>
2025-08-08 17:23:47 +0200

    add function for closing, response

--- src/js/order-modal.js
+++ src/js/order-modal.js
@@ -1,21 +1,21 @@
 import iziToast from "izitoast";
 import 'izitoast/dist/css/iziToast.min.css';
+import axios from "axios";
 
 const form = document.querySelector(".modal-form");
-const closeFormBtn = document.querySelector(".close-form-btn");
 const modalForm = document.querySelector(".order-modal");
 const emailForm = document.querySelector(".input-email");
 const commentForm = document.querySelector(".textarea");
 const phoneForm = document.querySelector(".input-phone");
 const plus = document.querySelector(".plus-elem");
 
+const savedData = localStorage.getItem("orderData");
+const parsedData = JSON.parse(savedData);
+const productId = parsedData.productId;
+const color = parsedData.color;
+
 // слухач події для форми
 form.addEventListener("submit", handleFormSubmit);
-
-// слухачі подій на закриття модалки (функції на закриття нижче поки тимчасові)
-closeFormBtn.addEventListener("click", handleCloseForm);
-modalForm.addEventListener("click", handleCloseModal);
-document.addEventListener("keydown", handleEscapeClose);
 
 // Сабміт на формі
 function handleFormSubmit(event) {
@@ -25,6 +25,14 @@ function handleFormSubmit(event) {
     const email = event.currentTarget.elements.email.value.trim();
     const phone = event.currentTarget.elements.phone.value.trim();
     const comment = event.currentTarget.elements.comment.value.trim();
+    const orderData = {
+        "email": email,
+        "phone": phone,
+        "modelId": productId,
+        "color": color,
+        "comment": comment
+    }
+    console.log(orderData);
 
     // помилка на емейлі (функція нижче)
     const existingEmailError = emailForm.parentElement.querySelector(".error-text");
@@ -73,6 +81,7 @@ function handleFormSubmit(event) {
             message: 'Форма успішно відправлена!',
             position: 'topCenter',
         });
+        submitOrder(orderData);
         form.reset();
     } else {
         iziToast.error({
@@ -92,22 +101,36 @@ function markupError(targetElement, message) {
     };
 }
 
-// Тимчасове закриття модалки всі три функції
-function handleEscapeClose(event) {
-  if (event.key === "Escape") {
-    modalForm.classList.add("visually-hidden");
-  }
+// відкриття модалки
+export function openOrderModal() {
+    document.body.classList.add('body--no-scroll');
+    modalForm.classList.remove('visually-hidden');
 }
 
-function handleCloseForm() {
-    modalForm.classList.add("visually-hidden");
+//  закриття модалки
+function closseOrderModal() {
+    document.body.classList.remove('body--no-scroll');
+    modalForm.classList.add('visually-hidden');
 }
 
-function handleCloseModal(event) {
-    if (event.target === modalForm) {
-        modalForm.classList.add("visually-hidden");
-    }
+// слухачі подій для закриття
+function listenerHandlClose() {
+    const closeFormBtn = document.querySelector(".close-form-btn");
+    closeFormBtn.addEventListener("click", closseOrderModal);
+
+
+    document.addEventListener("keydown", event => {
+        if (event.key === "Escape") {
+            closseOrderModal();
+        };
+    });
+    modalForm.addEventListener("click", event => {
+        if (event.target === modalForm) {
+            closseOrderModal();
+        }
+    });
 }
+ listenerHandlClose();
 
 // інпут у фокусі
 function elementInFocus(targetElement) {
@@ -136,3 +159,23 @@ function elementOutFocus(targetElement, placeholder) {
 elementOutFocus(emailForm, "12345ggg@gmail.com");
 elementOutFocus(commentForm, "Type your message...");
 elementOutFocus(phoneForm, "+38 (099) 123 22 11");
+
+// запит на бекенд
+const baseURL = "https://furniture-store.b.goit.study/api"
+async function submitOrder(orderData) {
+    try {
+        const response = await axios.post(`${baseURL}/orders`, orderData, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        console.log('Замовлення відправлено:', response.data);
+        return response;
+        
+    } catch (error) {
+        iziToast.error({
+            title: error.message,
+            position: 'topCenter',
+        });
+    }
+}
