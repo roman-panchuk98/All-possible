9778a3938019c8a6b9ddc9ad7621e2247bf9c0a5
BohdanVoitsehivsky <bogdanvoitsehivsky@gmail.com>
2025-08-08 18:54:09 +0300

    додав картинки на кнопки з категоріями і зробив кнопку Покахати ще, +частково стилізував

--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@ import 'modern-normalize/modern-normalize.css';
 import './css/styles.css';
 import './js/popular-goods';
 import { feedbackSection } from './js/feedback';
-import { handlerCategories } from './js/furniture_list';
+
 import refs from './js/refs';
+import { getCategories, getFurniture, handlerCategories, handlerMore} from './js/furniture_list';
 // import './js/order-modal.js'
 feedbackSection();
 // Furniture-list
 refs.categoriesList.addEventListener('click', handlerCategories);
+refs.furnitureLoadMoreBtn.addEventListener('click', handlerMore);
 
+
+getCategories();
+getFurniture(8, 1);
