8458ce1101f4f2fcdfbe8ca4f641baeb68c927bc
BohdanVoitsehivsky <bogdanvoitsehivsky@gmail.com>
2025-08-09 15:02:36 +0300

    виправив все по рекомендаціях тім-ліда і остаточно застилізував

--- src/main.js
+++ src/main.js
@@ -6,11 +6,7 @@ import { getCategories, getFurniture, handlerCategories, handlerMore} from './js
 import './js/feedback';
 import refs from './js/refs';
 import './js/order-modal.js'
-// Furniture-list
-refs.categoriesList.addEventListener('click', handlerCategories);
-refs.furnitureLoadMoreBtn.addEventListener('click', handlerMore);
-getCategories();
-getFurniture(8, 1);
+import './js/furniture_list.js';
 
 
 
