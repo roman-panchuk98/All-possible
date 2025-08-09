2721e91e7b3f888761421f0cffe19fb3b4cc157f
Anastasiia <gorgona1597531597@gmail.com>
2025-08-09 14:14:16 +0200

    fixed email and add refs

--- src/js/order-modal.js
+++ src/js/order-modal.js
@@ -1,17 +1,11 @@
 import iziToast from "izitoast";
 import 'izitoast/dist/css/iziToast.min.css';
 import axios from "axios";
-
-const form = document.querySelector(".modal-form");
-const modalForm = document.querySelector(".order-modal");
-const emailForm = document.querySelector(".input-email");
-const commentForm = document.querySelector(".textarea");
-const phoneForm = document.querySelector(".input-phone");
-
+import refs from "./refs";
 
 
 // слухач події для форми
-form.addEventListener("submit", handleFormSubmit);
+refs.modalOrderForm.addEventListener("submit", handleFormSubmit);
 
 // Сабміт на формі
 async function handleFormSubmit(event) {
@@ -53,36 +47,36 @@ async function handleFormSubmit(event) {
     };
 
     // помилка на емейлі (функція нижче)
-    const existingEmailError = emailForm.parentElement.querySelector(".error-text");
+    const existingEmailError = refs.modalOrderEmail.parentElement.querySelector(".error-text");
     if (!email || email.length > 64) {
-        emailForm.classList.add("error-message");
-        markupError(emailForm, "Не валідний Email");
+        refs.modalOrderEmail.classList.add("error-message");
+        markupError(refs.modalOrderEmail, "Не валідний Email");
     } else {
-        emailForm.classList.remove("error-message");
+        refs.modalOrderEmail.classList.remove("error-message");
         if (existingEmailError) {
             existingEmailError.remove();
         };
     };
 
     // помилка на телефоні якщо він порожній, якщо не валідний, то патерн і так не дає пройти, тому я вже не писала
-    const existingPhoneError = phoneForm.parentElement.querySelector(".error-text");
+    const existingPhoneError = refs.modalOrderPhone.parentElement.querySelector(".error-text");
     if (!phone) {
-        phoneForm.classList.add("error-message");
-        markupError(phoneForm, "Введіть номер телефону");
+        refs.modalOrderPhone.classList.add("error-message");
+        markupError(refs.modalOrderPhone, "Введіть номер телефону");
     } else {
-        phoneForm.classList.remove("error-message");
+        refs.modalOrderPhone.classList.remove("error-message");
         if (existingPhoneError) {
             existingPhoneError.remove();
         };
     };
 
     // помилка на комент
-    const existingCommentError = commentForm.parentElement.querySelector(".error-text");
+    const existingCommentError = refs.modalOrderComment.parentElement.querySelector(".error-text");
     if (comment.length < 5 || comment.length > 256) {
-        commentForm.classList.add("error-message");
-        markupError(commentForm, "Введіть коментар довжиною від 5 до 256 символів");
+        refs.modalOrderComment.classList.add("error-message");
+        markupError(refs.modalOrderComment, "Введіть коментар довжиною від 5 до 256 символів");
     } else {
-        commentForm.classList.remove("error-message");
+        refs.modalOrderComment.classList.remove("error-message");
         if (existingCommentError) {
             existingCommentError.remove();
         };
@@ -101,7 +95,7 @@ async function handleFormSubmit(event) {
                 message: 'Форма успішно відправлена!',
                 position: 'topCenter',
             });
-            form.reset();
+            refs.modalOrderForm.reset();
         }
     } else {
         iziToast.error({
@@ -124,13 +118,14 @@ function markupError(targetElement, message) {
 // відкриття модалки
 export function openOrderModal() {
     document.body.classList.add('body--no-scroll');
-    modalForm.classList.remove('visually-hidden');
+    refs.modalOrderBackground.classList.remove('visually-hidden');
 }
 
 //  закриття модалки
 function closseOrderModal() {
     document.body.classList.remove('body--no-scroll');
-    modalForm.classList.add('visually-hidden');
+    refs.modalOrderBackground.classList.add('visually-hidden');
+    localStorage.clear();
 }
 
 // слухачі подій для закриття
@@ -144,8 +139,8 @@ function listenerHandlClose() {
             closseOrderModal();
         };
     });
-    modalForm.addEventListener("click", event => {
-        if (event.target === modalForm) {
+    refs.modalOrderBackground.addEventListener("click", event => {
+        if (event.target === refs.modalOrderBackground) {
             closseOrderModal();
         }
     });
@@ -157,7 +152,7 @@ function elementInFocus(targetElement) {
     targetElement.addEventListener("focus", () => {
         targetElement.removeAttribute("placeholder");
 
-        if (targetElement === phoneForm) {
+        if (targetElement === refs.modalOrderPhone) {
             // Якщо немає префікса — додаємо один раз
             if (!targetElement.value.startsWith('+380')) {
                 targetElement.value = '+380';
@@ -165,16 +160,16 @@ function elementInFocus(targetElement) {
         }
     });
 
-    if (targetElement === phoneForm) {
+    if (targetElement === refs.modalOrderPhone) {
         // Слухач для вводу — додається лише один раз
         targetElement.addEventListener("input", () => {
-            phoneForm.value = "+380" + phoneForm.value.replace(/\D/g, '').slice(3);
+            refs.modalOrderPhone.value = "+380" + modalOrderPhone.value.replace(/\D/g, '').slice(3);
         });
     }
 }
-elementInFocus(emailForm);
-elementInFocus(commentForm);
-elementInFocus(phoneForm);
+elementInFocus(refs.modalOrderEmail);
+elementInFocus(refs.modalOrderComment);
+elementInFocus(refs.modalOrderPhone);
 
 // інпут не у фокусі
 function elementOutFocus(targetElement, placeholder) {
@@ -182,9 +177,9 @@ function elementOutFocus(targetElement, placeholder) {
         targetElement.setAttribute("placeholder", `${placeholder}`);
     })
 }
-elementOutFocus(emailForm, "12345ggg@gmail.com");
-elementOutFocus(commentForm, "Type your message...");
-elementOutFocus(phoneForm, "+38 (099) 123 22 11");
+elementOutFocus(refs.modalOrderEmail, "12345ggg@gmail.com");
+elementOutFocus(refs.modalOrderComment, "Type your message...");
+elementOutFocus(refs.modalOrderPhone, "+38 (099) 123 22 11");
 
 // запит на бекенд
 const baseURL = "https://furniture-store.b.goit.study/api"
--- src/js/refs.js
+++ src/js/refs.js
@@ -5,6 +5,11 @@ const refs = {
   furnitureLoadMoreBtn: document.querySelector('#furniture-loadMoreBtn'),
   modalDetailisProduct: document.querySelector('.modal'),
   popularGoodsList: document.querySelector('.popular-goods-list'),
+  modalOrderForm: document.querySelector(".modal-form"),
+  modalOrderBackground: document.querySelector(".order-modal"),
+  modalOrderEmail: document.querySelector(".input-email"),
+  modalOrderComment: document.querySelector(".textarea"),
+  modalOrderPhone: document.querySelector(".input-phone"),
 };
 
 export default refs;
